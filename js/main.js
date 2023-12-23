33e4e44e32d3ec18e9af73474b88a9658c66c0b2
Renan Cesar <cesar.renan@hotmail.com>
2023-12-22 21:19:50 -0300

    feat: adicionando o projeto

---
+++ js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function updateCountdown() {
+        const christmasDate = new Date('December 25, 2023 00:00:00').getTime();
+
+        const now = new Date().getTime();
+        const timeRemaining = christmasDate - now;
+
+        if (timeRemaining > 0) {
+            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+            document.getElementById('render').innerHTML = `${days}d -${hours < 10 ? '0' : ''}${hours}h -${minutes < 10 ? '0' : ''}${minutes}m -${seconds < 10 ? '0' : ''}${seconds}s`;
+        } else {
+            document.getElementById('render').innerHTML = 'Feliz Natal!';
+        }
+    }
+
+    setInterval(updateCountdown, 1000);
+
+    updateCountdown();
+});
